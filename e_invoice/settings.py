ad95d0fd3cc18dd7759c41b7050536402e4f4d7c
Waynelin1204 <linchienwei1204@gmail.com>
2025-02-27 14:54:32 +0800

    Renew

--- e_invoice/settings.py
+++ e_invoice/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-70occ2mn3ig)rm#3ev)cn%^^vv5$s8&(_w&$$4d1vi^$7og$@m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,"static/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
